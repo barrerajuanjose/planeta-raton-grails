b6e2ed382a56e1a82300a54da57872a18563afce
Juan Jose Barrera <barrerajuanjose@gmail.com>
2013-12-13 21:41:39 -0300

    add error.gsp y fix en hotel dao

--- src/groovy/com/planetaraton/dao/HotelDao.groovy
+++ src/groovy/com/planetaraton/dao/HotelDao.groovy
@@ -13,7 +13,7 @@ class HotelDao {
 	List getAll() {
 		List hotels = []
 		
-		getClass().getResource(hotelDirectory).getFile().eachFile {
+		grailsApplication.mainContext.getResource(hotelDirectory).getFile().eachFile {
 			hotels.add(new JsonSlurper().parse(new StringReader(it.getText())))
 		}
 		
